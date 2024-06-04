7aba100d9b323018357a877886f91c6c9dadc7f5
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-06-04 09:51:39 +0800

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pymongo
 from st_keyup import st_keyup
 from natural_lang_search import natural_search_main
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 st.set_page_config("Music Dictionary")
 @st.cache_resource
 def init_connection():
